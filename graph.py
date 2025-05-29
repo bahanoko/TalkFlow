4be33af05cadca3ef7a86c5a964483e064ea2aab
bahanoko <m.t0301m@icloud.com>
2025-05-30 00:57:30 +0900

    feat: add style.css

--- graph.py
+++ graph.py
@@ -4,15 +4,11 @@ from pyvis.network import Network
 class TopicGraph:
     def __init__(self):
         self.G = nx.Graph()
+        net = Network(height="600px", width="100%")
+        net.from_nx(self.G)    
+        net.write_html("./static/graph.html")
 
     def topics_to_graph(self, topics):
-        # for node in topics:
-        #     if isinstance(node, tuple):
-        #         topic, related_topics = node
-        #     else:
-        #         topic = node.topic
-        #         related_topics = node.related_topics
-
         for t in topics.topics:
             main_topic = t.topic
             self.G.add_node(main_topic)
@@ -21,7 +17,7 @@ class TopicGraph:
         self.visualize_graph_pyvis()
 
 
-    def visualize_graph_pyvis(self, output_html="graph.html"):
+    def visualize_graph_pyvis(self, output_html="./static/graph.html"):
         net = Network(height="600px", width="100%")
         net.from_nx(self.G)    
         net.write_html(output_html)
